fa7f0e55ddf29140b711d4f583245631b2d3e818
lausek <root@lausek.eu>
2017-10-11 10:01:26 +0100

    added mysql driver and connection

--- src/control/Connection.java
+++ src/control/Connection.java
@@ -1,29 +1,65 @@
 package control;
 
-public class Connection {
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class Connection {
+	
+	static {
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private java.sql.Connection sqlConnection;
+	private String host, user, password;
+	
 	/**
 	 * 
 	 * @param host
 	 * @param passwd
+	 * @throws Exception 
 	 */
-	public Connection(String host, String passwd) {
-		// TODO - implement Connection.Connection
-		//throw new UnsupportedOperationException();
+	public Connection(String hostString, String passwd) throws SQLException {
+		
+		String[] parts = hostString.split("@");
+		
+		user = !parts[0].isEmpty() ? parts[0] : "root";
+		host = parts.length > 1 ? parts[1] : "localhost";
+		password = passwd;
+		
+		sqlConnection = DriverManager.getConnection("jdbc:mysql://"+host, user, password);
+		
+		//TODO: Add null check?
+		
 	}
-
-	/**
-	 * 
-	 * @param host
-	 */
-	public Connection(String host) {
-		// TODO - implement Connection.Connection
-		throw new UnsupportedOperationException();
+	
+	//TODO: Is this needed?
+	public Connection(String host) throws SQLException {
+		this(host, "");
 	}
 
 	public void logout() {
-		// TODO - implement Connection.logout
-		throw new UnsupportedOperationException();
+		try {
+			sqlConnection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Statement newStatement() throws SQLException {
+		return sqlConnection.createStatement();
+	}
+	
+	public String getHost() {
+		return host;
+	}
+	
+	public String getUser() {
+		return user;
 	}
 
 }
--- src/view/SelectionScreen.java
+++ src/view/SelectionScreen.java
@@ -1,10 +1,18 @@
 package view;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
+
+import view.Display.EnumFatality;
 
 @SuppressWarnings("serial")
 public class SelectionScreen extends Screen {
-
+	
+	private List<String> databases = new ArrayList<>();
+	
 	public SelectionScreen(Display parent) {
 		super(parent);
 	}
@@ -12,6 +20,30 @@ public class SelectionScreen extends Screen {
 	@Override
 	public void build() {
 		super.build();
+	}
+	
+	@Override
+	public void onEnter() {
+		super.onEnter();
+		
+		// Load available databases from SQL server
+		try {
+			
+			ResultSet result = parent
+				.getControl()
+				.getConnection()
+				.newStatement()
+				.executeQuery("SHOW DATABASES");
+			
+			while(result.next()) {
+				System.out.println(result.getString(1));
+				databases.add(result.getString(1));
+			}
+			
+		} catch(SQLException e) {
+			parent.notice(EnumFatality.ERROR, "Couldn't fetch databases from server");
+		}
+		
 	}
 	
 	@Override
