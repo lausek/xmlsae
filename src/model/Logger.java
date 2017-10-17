db993433fe86780b464ef8e32fae71cfa038f712
lausek <root@lausek.eu>
2017-10-17 10:37:05 +0200

    removed some todos; formatting

--- src/model/Logger.java
+++ src/model/Logger.java
@@ -8,29 +8,27 @@ import org.apache.log4j.PatternLayout;
 import org.apache.log4j.RollingFileAppender;
 
 public class Logger {
-	
+
 	public Logger() {
-		
+
 	}
 
-	public org.apache.log4j.Logger initLogger(org.apache.log4j.Logger logger, String logFile){
+	public org.apache.log4j.Logger initLogger(org.apache.log4j.Logger logger, String logFile) {
 		PatternLayout layout = new PatternLayout("%d %p - %m%n");
-		ConsoleAppender consoleAppender = new ConsoleAppender( layout );
-		logger.addAppender( consoleAppender );
+		ConsoleAppender consoleAppender = new ConsoleAppender(layout);
+		logger.addAppender(consoleAppender);
 		RollingFileAppender fileAppender;
 		try {
-			fileAppender = new RollingFileAppender(layout, logFile); //"tmp/Zolltarifnummer.log"
+			fileAppender = new RollingFileAppender(layout, logFile); // "tmp/Zolltarifnummer.log"
 			fileAppender.setMaxFileSize("10MB");
 			fileAppender.setMaxBackupIndex(10);
-			logger.addAppender( fileAppender );
+			logger.addAppender(fileAppender);
 			// ALL | DEBUG | INFO | WARN | ERROR | FATAL | OFF
-			logger.setLevel( Level.DEBUG);
-		} catch (IOException e){
+			logger.setLevel(Level.DEBUG);
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return logger;
 	}
-	
-	
 
 }
--- src/view/atoms/CListItem.java
+++ src/view/atoms/CListItem.java
@@ -5,9 +5,7 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
-import java.awt.image.BufferedImageFilter;
 import java.io.IOException;
 
 import javax.swing.JLabel;
