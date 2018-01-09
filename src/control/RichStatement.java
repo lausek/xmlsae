08cfccec9c280a8bb857c2a7f5e6c4813723cfa1
lausek <root@lausek.eu>
2018-01-09 14:15:35 +0100

    prevent sql injection in RichStatement

--- src/control/RichStatement.java
+++ src/control/RichStatement.java
@@ -3,23 +3,33 @@ package control;
 import java.sql.SQLException;
 import java.util.List;
 
-public class RichStatement {
+import org.owasp.esapi.ESAPI;
+import org.owasp.esapi.codecs.Codec;
+import org.owasp.esapi.codecs.MySQLCodec;
+import org.owasp.esapi.codecs.MySQLCodec.Mode;
 
+public class RichStatement {
+	
+	private static Codec codec;
+	
 	private String query;
 	private List<String> params;
-
+	
+	static {
+		codec = new MySQLCodec(Mode.STANDARD);
+	}
+	
 	public RichStatement(String query) throws SQLException {
 		this.query = query;
 		this.params = new java.util.ArrayList<>();
 	}
 
 	public void setRaw(String raw) throws SQLException {
-		// TODO: implement
-		params.add(raw);
+		params.add(ESAPI.encoder().encodeForSQL(codec, raw));
 	}
 
 	public void setString(String val) throws SQLException {
-		params.add('"' + val + '"');
+		params.add('"' + ESAPI.encoder().encodeForSQL(codec, val) + '"');
 	}
 
 	public void executeUpdate() throws SQLException {
